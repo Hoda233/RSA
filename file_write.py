9c3a4b3ef5fd9ad52887c7bc629cb1935cd2718e
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2023-04-12 20:47:29 +0200

    add tests

--- file_write.py
+++ file_write.py
@@ -1,5 +1,5 @@
 with open("effiency_test.txt", "w") as f:
-    for i in range(28, 2049):
+    for i in range(28, 1025):#1025,2049
         f.write(str(i) + "\n")
         f.write("hello\n")
 f.close()
